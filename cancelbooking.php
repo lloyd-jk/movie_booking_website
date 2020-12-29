926ea0caf4dce3b7aeaeed7cbd201914dd5ad759
Abhinav-Unniyattil <abhinavunniyattil00@gmail.com>
2020-12-29 18:25:41 +0400

    Update cancelbooking.php
    Added styling

--- cancelbooking.php
+++ cancelbooking.php
@@ -50,8 +50,8 @@ $query = mysqli_query($connect,$sql) or die(mysqli_error($connect,$sql));
 <header id="site-header" class="w3l-header fixed-top">
 		<nav class="navbar navbar-expand-lg navbar-light fill px-lg-0 py-0 px-3">
 			<div class="container">
-				<h1><a class="navbar-brand" href="index.html"><span class="fa fa-film"
-							aria-hidden="true"></span> BookYourShow </a></h1>
+				<h1><a class="navbar-brand" href="index.php"><span class="fa fa-film" aria-hidden="true"></span>
+							BookYourShow </a></h1>
 				<!-- if logo is image enable this   
 						<a class="navbar-brand" href="#index.html">
 							<img src="image-path" alt="Your logo" title="Your logo" style="height:35px;" />
@@ -66,16 +66,12 @@ $query = mysqli_query($connect,$sql) or die(mysqli_error($connect,$sql));
 
 				<div class="collapse navbar-collapse" id="navbarSupportedContent">
 					<ul class="navbar-nav ml-auto">
-						<li class="nav-item active">
+						<li class="nav-item">
 							<a class="nav-link" href="index.php">Home</a>
 						</li>
 						<li class="nav-item">
 							<a class="nav-link" href="mybooking.php">My Bookings</a>
 						</li>
-						<!-- <li class="nav-item">
-							<a class="nav-link" href="genre.php">Genre</a>
-						</li> -->
-
 						<li class="nav-item">
 							<a class="nav-link" href="contact.php">Contact</a>
 						</li>
@@ -83,9 +79,9 @@ $query = mysqli_query($connect,$sql) or die(mysqli_error($connect,$sql));
 
 					<!--/search-right-->
 					<!--/search-right-->
-					<!-- <div class="search-right"> -->
-						<!-- <a href="#search" class="btn search-hny mr-lg-3 mt-lg-0 mt-4" title="search">Search <span -->
-								<!-- class="fa fa-search ml-3" aria-hidden="true"></span></a> -->
+					<!-- <div class="search-right">
+						<a href="#search" class="btn search-hny mr-lg-3 mt-lg-0 mt-4" title="search">Search <span
+								class="fa fa-search ml-3" aria-hidden="true"></span></a> -->
 						<!-- search popup -->
 						<!-- <div id="search" class="pop-overlay">
 							<div class="popup">
@@ -110,7 +106,7 @@ $query = mysqli_query($connect,$sql) or die(mysqli_error($connect,$sql));
 										<li><a href="genre.html">Drama</a></li>
 										<li><a href="genre.html">Family</a></li>
 										<li><a href="genre.html">Thriller</a></li>
-										<li><a href="genre.html">Commedy</a></li>
+										<li><a href="genre.html">Comedy</a></li>
 										<li><a href="genre.html">Romantic</a></li>
 										<li><a href="genre.html">Tv-Series</a></li>
 										<li><a href="genre.html">Horror</a></li>
@@ -157,27 +153,24 @@ $query = mysqli_query($connect,$sql) or die(mysqli_error($connect,$sql));
 				<!-- Drop down ends -->
 			
 		</nav>
-	</header>
+    </header>
 
 
 
 	<!-- //banner-slider-->
 	<!-- main-slider -->
 	<!--grids-sec1-->
-	<br><br><br><br><br><br><br>
-	<section class="w3l-grids" style="text-align:center;position:relative;left:300px">
-		<div class="grids-main py-5">
-			<div class="container py-lg-3">
+	<section class="w3l-grids">
+		<div class="grids-main mt-5 py-5">
+			<div class="container mt-5 py-lg-3">
 				<div class="headerhny-title">
-					<div class="w3l-title-grids">
-						<div class="headerhny-left">
-						<h3 class="hny-title"><?php echo $temp . ", your booking with ID number: " . $delete_term . " has been cancelled."; ?></h3>
-                        <a href="booked.php">CLick here to go back to the booked tickets page</a>
+						<div class="headerhny-title text-center">
+						<h3 class="sub-title"><?php echo $temp . ", your booking with ID number: " . $delete_term . " has been cancelled."; ?></h3>
+                        <!-- <a href="booked.php">CLick here to go back to the booked tickets page</a> -->
 						</div>
 						<!--<div class="headerhny-right text-lg-right">
 							<h4><a class="show-title" href="genre.html">Show all</a></h4>
 						</div>-->
-					</div>
                 </div><br><br><br>
 				</div>
 			</div>
@@ -190,98 +183,95 @@ $query = mysqli_query($connect,$sql) or die(mysqli_error($connect,$sql));
     ?>
 	<!-- //tabs-->
 	<!-- footer-66 -->
-	<!-- <footer class="w3l-footer">
-		<section class="footer-inner-main">
-			<div class="footer-hny-grids py-5">
-				<div class="container py-lg-4">
-					<div class="text-txt">
-						<div class="right-side"> -->
-							<!-- <div class="row footer-about">
-								<div class="col-md-3 col-6 footer-img mb-lg-0 mb-4">
-									<a href="genre.html"><img class="img-fluid" src="assets/images/banner1.jpg"
-											alt=""></a>
+	<footer class="w3l-footer">
+			<section class="footer-inner-main">
+				<div class="footer-hny-grids py-5">
+					<div class="container py-lg-4">
+						<div class="text-txt">
+							<div class="right-side">
+								<div class="row footer-about">
+									<div class="col-md-3 col-6 footer-img mb-lg-0 mb-4">
+										<a href="articles.html"><img class="img-fluid" src="assets/images/banner1.jpg"
+												alt=""></a>
+									</div>
+									<div class="col-md-3 col-6 footer-img mb-lg-0 mb-4">
+										<a href="articles.html"><img class="img-fluid" src="assets/images/banner2.jpg"
+												alt=""></a>
+									</div>
+									<div class="col-md-3 col-6 footer-img mb-lg-0 mb-4">
+										<a href="articles.html"><img class="img-fluid" src="assets/images/banner3.jpg"
+												alt=""></a>
+									</div>
+									<div class="col-md-3 col-6 footer-img mb-lg-0 mb-4">
+										<a href="articles.html"><img class="img-fluid" src="assets/images/banner4.jpg"
+												alt=""></a>
+									</div>
 								</div>
-								<div class="col-md-3 col-6 footer-img mb-lg-0 mb-4">
-									<a href="genre.html"><img class="img-fluid" src="assets/images/banner2.jpg"
-											alt=""></a>
-								</div>
-								<div class="col-md-3 col-6 footer-img mb-lg-0 mb-4">
-									<a href="genre.html"><img class="img-fluid" src="assets/images/banner3.jpg"
-											alt=""></a>
-								</div>
-								<div class="col-md-3 col-6 footer-img mb-lg-0 mb-4">
-									<a href="genre.html"><img class="img-fluid" src="assets/images/banner4.jpg"
-											alt=""></a>
-								</div>
-							</div>
-							<div class="row footer-links"> -->
-
-
-								
-								<!-- <div class="col-md-3 col-sm-6 sub-two-right mt-5">
-									<h6>Information</h6>
-									<ul>
-										<li><a href="index.php">Home</a> </li>
-										<li><a href="about.php">About</a> </li>
-										<li><a href="main login.php">Login</a></li>
-										<li><a href="contact.php">Contact</a></li>
-									</ul>
-								</div>
-								
-								<div class="col-md-3 col-sm-6 sub-two-right mt-5">
-									<h6>Newsletter</h6>
-									<form action="#" class="subscribe mb-3" method="post">
-										<input type="email" name="email" placeholder="Your Email Address" required="">
-										<button><span class="fa fa-envelope-o"></span></button>
-									</form>
-									<p>Enter your email and receive the latest news, updates and special offers from us.
-									</p>
+								<!-- <div class="row footer-links">
+									
+									<div class="col-md-3 col-sm-6 sub-two-right mt-5">
+										<h6>Information</h6>
+										<ul>
+											<li><a href="index.php">Home</a> </li>
+											<li><a href="about.php">About</a> </li>
+											<li><a href="main login.php">Login</a></li>
+											<li><a href="contact.php">Contact</a></li>
+										</ul>
+									</div>
+									
+									<div class="col-md-3 col-sm-6 sub-two-right mt-5">
+										<h6>Newsletter</h6>
+										<form action="#" class="subscribe mb-3" method="post">
+											<input type="email" name="email" placeholder="Your Email Address" required="">
+											<button><span class="fa fa-envelope-o"></span></button>
+										</form>
+										<p>Enter your email and receive the latest news, updates and special offers from us.
+										</p>
+									</div>
 								</div> -->
-							<!-- </div>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-			</div>
-			<br><br> -->
-			<!-- <div class="below-section">
-				<div class="container">
-					<div class="copyright-footer">
-						<div class="columns text-lg-left">
-							<p> Designed by Group XVII</p>
+				</div>
+				<div class="below-section">
+					<div class="container">
+						<div class="copyright-footer">
+							<div class="columns text-lg-left">
+								<p>Designed by Group XVII</p>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div> -->
-			<!-- copyright -->
-			<!-- move top -->
-			<!-- <button onclick="topFunction()" id="movetop" title="Go to top">
-				<span class="fa fa-arrow-up" aria-hidden="true"></span>
-			</button> -->
-			<!-- <script>
-				// When the user scrolls down 20px from the top of the document, show the button
-				window.onscroll = function () {
-					scrollFunction()
-				};
+				<!-- copyright -->
+				<!-- move top -->
+				<button onclick="topFunction()" id="movetop" title="Go to top">
+					<span class="fa fa-arrow-up" aria-hidden="true"></span>
+				</button>
+				<script>
+					// When the user scrolls down 20px from the top of the document, show the button
+					window.onscroll = function () {
+						scrollFunction()
+					};
 
-				function scrollFunction() {
-					if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-						document.getElementById("movetop").style.display = "block";
-					} else {
-						document.getElementById("movetop").style.display = "none";
+					function scrollFunction() {
+						if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+							document.getElementById("movetop").style.display = "block";
+						} else {
+							document.getElementById("movetop").style.display = "none";
+						}
 					}
-				}
 
-				// When the user clicks on the button, scroll to the top of the document
-				function topFunction() {
-					document.body.scrollTop = 0;
-					document.documentElement.scrollTop = 0;
-				}
-			</script> -->
-			<!-- /move top -->
+					// When the user clicks on the button, scroll to the top of the document
+					function topFunction() {
+						document.body.scrollTop = 0;
+						document.documentElement.scrollTop = 0;
+					}
+				</script>
+				<!-- /move top -->
 
-		<!-- </section> -->
-	<!-- </footer> -->
+		</section>
+	</footer>
 	<!--//footer-66 -->
 <!--
     <?php
