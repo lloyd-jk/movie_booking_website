833c11cc4487df86606474977d36b37d2b57c29e
Abhinav-Unniyattil <abhinavunniyattil00@gmail.com>
2020-12-29 14:43:57 +0400

    Update display_theater.php
    BookYourShow link and Admin Panel breadcrumb

--- display_theater.php
+++ display_theater.php
@@ -55,7 +55,7 @@ $query = mysqli_query($connect,$sql) or die(mysqli_error($connect));
 			<!--/nav-->
 			<nav class="navbar navbar-expand-lg navbar-light fill px-lg-0 py-0 px-3">
 				<div class="container">
-					<h1><a class="navbar-brand" href="index.html"><span class="fa fa-film" aria-hidden="true"></span>
+					<h1><a class="navbar-brand" href="select_theatre.php"><span class="fa fa-film" aria-hidden="true"></span>
 					BookYourShow </a></h1>
 					<!-- if logo is image enable this   
 							<a class="navbar-brand" href="#index.html">
@@ -139,8 +139,6 @@ $query = mysqli_query($connect,$sql) or die(mysqli_error($connect));
 			<nav id="breadcrumbs" class="breadcrumbs">
 				<div class="container page-wrapper">
                 <a href="select_theatre.php">Home</a> » <span class="breadcrumb_last" aria-current="page">View Bookings</span>
-                <h5 style="float:right;font-family: 'Open Sans', sans-serif; font-weight: 400;"><i class="fa fa-lock" aria-hidden="true"></i>
-Admin Panel</h5>
                 </div>
 			</nav>
 		</div>
