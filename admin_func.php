104293e1c72d812ea7619b88993a74bc5f362e4d
Abhinav-Unniyattil <abhinavunniyattil00@gmail.com>
2020-12-29 14:29:35 +0400

    Update admin_func.php
    BookYourShow link and removed Admin Panel from breadcrumb

--- admin_func.php
+++ admin_func.php
@@ -27,7 +27,7 @@
 			<!--/nav-->
 			<nav class="navbar navbar-expand-lg navbar-light fill px-lg-0 py-0 px-3">
 				<div class="container">
-					<h1><a class="navbar-brand" href="index.html"><span class="fa fa-film" aria-hidden="true"></span>
+					<h1><a class="navbar-brand" href="select_theatre.php"><span class="fa fa-film" aria-hidden="true"></span>
 					BookYourShow </a></h1>
 					<!-- if logo is image enable this   
 							<a class="navbar-brand" href="#index.html">
@@ -111,8 +111,6 @@
 			<nav id="breadcrumbs" class="breadcrumbs">
 				<div class="container page-wrapper">
 				<a href="select_theatre.php">Home</a> » <span class="breadcrumb_last" aria-current="page">Select Operation</span>
-				<h5 style="float:right;font-family: 'Open Sans', sans-serif; font-weight: 400;"><i class="fa fa-lock" aria-hidden="true"></i>
-Admin Panel</h5>
 				</div>
 			</nav>
 		</div>
