178abd129c88c0f02493091487af8f3d7965210c
Abhinav-Unniyattil <abhinavunniyattil00@gmail.com>
2020-12-30 08:02:06 +0400

    Update delete.php
    removed comments

--- delete.php
+++ delete.php
@@ -8,12 +8,6 @@
         $t_id = $_GET['id'];
         $movie = mysqli_real_escape_string($conn, $_POST['movies']);
 
-        // $sql1 = "SELECT movie_id FROM 'movie' WHERE movie_title = '$movie'";
-        // $movie_id = mysqli_query($conn,$sql1);
-
-        // $sql2 = "SELECT t_id FROM 'theatre' WHERE t_name = '$tName'";
-        // $t_id = mysqli_query($conn,$sql2);
-
         $sql = "DELETE FROM `theatre-movie` WHERE `theatre-movie`.`t_id` = $t_id AND `theatre-movie`.`movie_id` = $movie";
         $res = mysqli_query($conn,$sql);
 
